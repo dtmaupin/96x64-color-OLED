edb06748d817bb6128800b23adca30b653dc0038
Daniel Maupin <dmaupin@live.com>
2018-05-07 17:46:54 -0400

    modified:   96x64-color-OLED.ino updates only on new readings

--- 96x64-color-OLED.ino
+++ 96x64-color-OLED.ino
@@ -14,24 +14,13 @@
 #include <SPI.h>
 #include "Ucglib.h"
 
-  float RH;
-  float RH_current;
-  float RH_Logic;
-  double TempC;
-  long lastreading = "0";
-Adafruit_Si7021 sensor = Adafruit_Si7021();
 
-    
-/*
-  Hardware SPI Pins:
-    Arduino Uno		sclk=13, data=11
-    Arduino Due		sclk=76, data=75
-    Arduino Mega	sclk=52, data=51
-    
-  >>> Please uncomment (and update) one of the following constructors. <<<  
-*/
+  float RH_current;  //set current reading floating point
+  int RH_round;  //set int for rounded reading
+  int RH_last;  //set int point number for comparions logic
 
-Ucglib_SSD1331_18x96x64_UNIVISION_SWSPI ucg(/*sclk=*/ 52, /*data=*/ 51, /*cd=*/ 48, /*cs=*/ 49, /*reset=*/ 50);
+Adafruit_Si7021 sensor = Adafruit_Si7021();  //Setup Sensor Type
+Ucglib_SSD1331_18x96x64_UNIVISION_SWSPI ucg(/*sclk=*/ 52, /*data=*/ 51, /*cd=*/ 48, /*cs=*/ 49, /*reset=*/ 50);  //Setup Display and SPI pins
 
 void setup(void)
 {
@@ -48,43 +37,37 @@ void setup(void)
     delay(10);
   }
 
-  Serial.println("Si7021 test!");
-  Serial.print("RH Debug Value:"); Serial.println(RH);
+  Serial.println("Starting up sensor type: Si7021");
   
   if (!sensor.begin()) {
     Serial.println("Did not find Si7021 sensor!");
     while (true);
   }
-}
-
-void loop(void)
-{
+  //Write fixed text to display
   //ucg.setRotate90();
-  
-  
   ucg.setFont(ucg_font_ncenR10_tr);
   ucg.setColor(0, 50, 240);
   ucg.setPrintPos(0,15);
   ucg.print("RH%: "); 
-  float RH_current = sensor.readHumidity();
-  Serial.print("RH_Logic before if: "); Serial.println(RH_Logic); //More debug
-  Serial.print("RH_current before if: "); Serial.println(RH_current); //More debug
-  if(RH_current != RH_Logic) {
-    ucg.setColor(0, 0, 0);
-    ucg.drawBox(40, 0, 38, 20);
-    ucg.setColor(0, 255, 0);
-    ucg.print(RH_current);
-    ucg.print("%");
-    Serial.print("RH_current: "); Serial.println(RH_current); // Serial debug info
-    RH_Logic = RH_current;
-    Serial.print("RH_Logic: "); Serial.println(RH_Logic); //More debug
-    delay(1000);    
+}
+
+void loop(void)
+{
+  float RH_current = sensor.readHumidity(); //Read sensor data as float
+  RH_round=round(RH_current*100);           //Round current reading*100
+  if(RH_round != RH_last) {                 //If the rounded current reading and the last reading
+    ucg.setPrintPos(40,15);                 //Set starting postion for updating display
+    ucg.setColor(0, 0, 0);                  //Set color to black for clearing box
+    ucg.drawBox(40, 0, 38, 20);             //Draw a box from 40,0 to 38,20 solid black
+    ucg.setColor(0, 255, 0);                //Set color to green.  May add logic for color based on value later
+    ucg.print(RH_current);                  //Update the current readint to the display
+    ucg.print("%");                         //Print a % this could be moved to static text later.
+    RH_last = round(RH_current*100);        //Round last reading and set it to RH_last
+    delay(1000);                            //Delay 1 sec since the sensor updates fast enough for the display to flicker with new values
   } else {
   //RH = sensor.readHumidity();
   //float TempC=sensor.readTemperature();
   delay(1000);  
   
   }
-  Serial.print("RH_Logic after if: "); Serial.println(RH_Logic); //More debug
-  Serial.print("RH_current after if: "); Serial.println(RH_current); //More debug
 }
